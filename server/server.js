582524873b43dccb6532d87155673c69f2f829df
kevinsounthavong <soun042564@gmail.com>
2018-04-21 18:25:34 -0400

    Updated PORT variable

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 const path = require('path');
 // __dirname is server folder in this context
-const publicPath = path.join(__dirname,"../public");
+const publicPath = path.join(__dirname, "../public");
 const express = require('express');
+const port = process.env.PORT || 3000; // set up for Heroku
 const app = express();
 
-const port = process.env || 3000; // set up for Heroku
 //console.log(__dirname + '/../public');  // old way to get public folder
 
 app.use(express.static(publicPath)); // serve static files
 
 
-app.listen(port, () => console.log(`Server listening on port ${port}!`));
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
